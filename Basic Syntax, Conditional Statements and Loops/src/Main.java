3f46e2c3d74c24f3575266a019bf7ea744fcd4a6
Nikolay Daskalov <daskalovnikolaj@gmail.com>
2021-03-29 06:16:53 +0300

    Update

--- Basic Syntax, Conditional Statements and Loops/src/Main.java
+++ Basic Syntax, Conditional Statements and Loops/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f.", name, age, grade);
     }
 }
