2eab7a37bd11365bc82bd042adfa690a2d10172a
Kmetamazter <daskalovnikolaj@gmail.com>
2021-03-30 05:01:30 +0300

    Commit

--- Basic Syntax, Conditional Statements and Loops/p04_Foreign_Languages/src/Main.java
+++ Basic Syntax, Conditional Statements and Loops/p04_Foreign_Languages/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("helllo");
+        Scanner scanner = new Scanner(System.in);
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "USA":
+            case "England":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Mexico":
+            case "Argentina":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+        }
     }
 }
