a58f5a49edbda2351ae6a72b018b6582da837894
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-09 15:23:06 +0300

    Commit

--- Methods/p01_SignOfIntegerNumbers/src/Main.java
+++ Methods/p01_SignOfIntegerNumbers/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        numberRange(n);
+    }
+
+    public static void numberRange(int number) {
+        if (number > 0) {
+            System.out.printf("The number %d is positive.%n", number);
+        } else if (number < 0) {
+            System.out.printf("The number %d is negative.%n", number);
+        } else {
+            System.out.println("The number 0 is zero.");
+        }
     }
 }
