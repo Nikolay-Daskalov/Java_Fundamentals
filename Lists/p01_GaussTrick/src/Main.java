ed94caf66587b7c7a772f6e82c689edaa9c0bb2f
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-13 07:58:16 +0300

    Commit

--- Lists/p01_GaussTrick/src/Main.java
+++ Lists/p01_GaussTrick/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String[] numbers = scanner.nextLine().split(" ");
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            nums.add(Integer.parseInt(numbers[i]));
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            int first = nums.get(i);
+            int last = nums.get(nums.size() - 1);
+
+            if (i != nums.size() - 1) {
+                nums.remove(i);
+                nums.remove(nums.size() - 1);
+                nums.add(i, first + last);
+            }
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.print(nums.get(i) + " ");
+        }
     }
 }
