3033f4d666394d856a8cb7b0d497ee009f3ed169
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-13 07:18:55 +0300

    Update Main.java

--- Methods/p05_CalculateRectangleArea/src/Main.java
+++ Methods/p05_CalculateRectangleArea/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the height: ");
+        double height = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Enter the width: ");
+        double width = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("The area is: " + calculateRectangleArea(height, width));
+    }
+
+    private static double calculateRectangleArea(double height, double width) {
+        return height * width;
     }
 }
