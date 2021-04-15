63580483e3161284a4c5e6702f08c9ab99fb9555
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-15 18:05:06 +0300

    Update Main.java

--- Objects and Classes/p01_RandomizeWords/src/Main.java
+++ Objects and Classes/p01_RandomizeWords/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> input = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
+
+        Random random = new Random();
+
+        for (int i = 0; i < input.size(); i++) {
+            int num = random.nextInt(input.size());
+            String swap = input.get(num);
+            input.set(num, input.get(i));
+            input.set(i, swap);
+        }
+
+        for (int i = 0; i < input.size(); i++) {
+            System.out.println(input.get(i));
+        }
     }
 }
