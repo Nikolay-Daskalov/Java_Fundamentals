0a4a26a2e38b5e6beda80a249f8613250c93ada8
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-17 10:25:20 +0300

    Added classes

---
+++ Objects and Classes/p03_Students/src/Student.java
@@ -0,0 +1,30 @@
+public class Student {
+
+    String firstName;
+    String lastName;
+    int age;
+    String hometown;
+
+    public Student(String firstName, String lastName, int age, String hometown) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.hometown = hometown;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getHometown() {
+        return this.hometown;
+    }
+}
