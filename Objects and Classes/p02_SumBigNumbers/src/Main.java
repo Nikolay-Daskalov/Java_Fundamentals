36462b19eae3fb0f94bd33b0c5e658d7b148c23d
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-17 10:18:30 +0300

    Update Main.java

--- Objects and Classes/p02_SumBigNumbers/src/Main.java
+++ Objects and Classes/p02_SumBigNumbers/src/Main.java
@@ -1,5 +1,18 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        BigInteger a = new BigInteger(scanner.nextLine());
+
+        System.out.print("Enter second integer: ");
+        BigInteger b = new BigInteger(scanner.nextLine());
+
+        BigInteger sum = a.add(b);
+
+        System.out.printf("The sum is: %s", sum.toString());
     }
 }
