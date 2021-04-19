df5b603841251c570cc653646f5bcda9fb674dae
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-19 18:27:07 +0300

    Update

--- Objects and Classes/p07_OrderByAge/src/Main.java
+++ Objects and Classes/p07_OrderByAge/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+
+        List<Person> people = new ArrayList<>();
+
+        while (!input[0].equals("End")) {
+            Person person = new Person(input[0], input[1], Integer.parseInt(input[2]));
+            people.add(person);
+            input = scanner.nextLine().split("\\s+");
+        }
+
+        people.stream()
+                .sorted((a, b) -> Integer.compare(b.getAge(), a.getAge()))
+                .forEach(person -> System.out.println(person.toString()));
     }
 }
