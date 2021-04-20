51a13c1d831c55bbdf1cbcdb9d61bf036e6a88c0
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-20 06:16:29 +0300

    Update Main.java

--- Maps, Lambda and Stream API/p02_WordSynonyms/src/Main.java
+++ Maps, Lambda and Stream API/p02_WordSynonyms/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Map<String, String> map = new LinkedHashMap<>();
+        for (int i = 0; i < n; i++) {
+            String key = scanner.nextLine();
+            String value = scanner.nextLine();
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + ", " + value);
+            } else {
+                map.put(key, value);
+            }
+        }
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(), entry.getValue());
+        }
     }
 }
