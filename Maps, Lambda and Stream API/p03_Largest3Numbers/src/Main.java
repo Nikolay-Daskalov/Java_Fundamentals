16e633f42d1df72185b56d365f0a2e19b8a7def3
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-20 06:28:53 +0300

    Update Main.java

--- Maps, Lambda and Stream API/p03_Largest3Numbers/src/Main.java
+++ Maps, Lambda and Stream API/p03_Largest3Numbers/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .forEach(integer -> System.out.print(integer + " "));
     }
 }
