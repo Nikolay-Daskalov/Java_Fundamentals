064f71236fe534fe50f7ab7536247d6bdfe5e794
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-20 06:11:30 +0300

    Update Main.java

--- Maps, Lambda and Stream API/p01_Count Real Numbers/src/Main.java
+++ Maps, Lambda and Stream API/p01_Count Real Numbers/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        Map<Double, Integer> map = new TreeMap<>();
+
+        double[] nums = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToDouble(Double::parseDouble).toArray();
+
+        for (double num : nums) {
+            map.putIfAbsent(num, 0);
+            map.put(num, map.get(num) + 1);
+        }
+        Consumer<Map.Entry<Double, Integer>> consumer = entry -> System.out.printf("%.2f -> %d.%n", entry.getKey(), entry.getValue());
+
+        map.entrySet().stream()
+                .forEach(consumer);
     }
 }
