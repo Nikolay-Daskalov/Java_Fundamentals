6e71820f92a61ae84f272898effba9259494ed01
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-21 20:09:01 +0300

    Update Main.java

--- Maps, Lambda and Stream API/p06_StudentAcademy/src/Main.java
+++ Maps, Lambda and Stream API/p06_StudentAcademy/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, List<Double>> students = new LinkedHashMap<>();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter name: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Enter grade: ");
+            double grade = Double.parseDouble(scanner.nextLine());
+
+            if (students.containsKey(name)) {
+                List<Double> grades = students.get(name);
+                grades.add(grade);
+                students.put(name, grades);
+            } else {
+                students.put(name, new ArrayList<>(Collections.singletonList(grade)));
+            }
+        }
+
+        Output(students);
+    }
+
+    private static void Output(Map<String, List<Double>> students) {
+        students.entrySet().stream()
+                .filter(grade -> grade.getValue().stream().mapToDouble(d -> d).sum() > 4.50)
+                .sorted((a, b) -> Double.compare(b.getValue().stream().mapToDouble(d -> d).sum(),
+                        a.getValue().stream().mapToDouble(d -> d).sum()))
+                .forEach(grade -> System.out.printf("%s -> %.2f%n", grade.getKey(),
+                        grade.getValue().stream().mapToDouble(a -> a).average().getAsDouble()));
     }
 }
