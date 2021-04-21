c0add9fc75b1e2c5087cc5b24bdfc1cd5a2bf85a
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-21 13:51:01 +0300

    Update Main.java

--- Maps, Lambda and Stream API/p04_CountCharsInAString/src/Main.java
+++ Maps, Lambda and Stream API/p04_CountCharsInAString/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Map<Character, Integer> charactersCount = new LinkedHashMap<>();
+
+        for (int i = 0; i < input.trim().length(); i++) {
+            if (input.charAt(i) != ' ') {
+                if (charactersCount.containsKey(input.charAt(i))) {
+                    charactersCount.put(input.charAt(i), charactersCount.get(input.charAt(i)) + 1);
+                } else {
+                    charactersCount.put(input.charAt(i), 1);
+                }
+            }
+        }
+        charactersCount.entrySet().stream()
+                .forEach(characterCount -> System.out.println(characterCount.getKey() + " -> " + characterCount.getValue()));
     }
 }
