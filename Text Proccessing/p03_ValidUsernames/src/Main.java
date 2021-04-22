845f34719c7e512c4dc8467d54e8cb504830e092
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-22 08:46:27 +0300

    Update Main.java

--- Text Proccessing/p03_ValidUsernames/src/Main.java
+++ Text Proccessing/p03_ValidUsernames/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter usernames to check: ");
+        String[] tokens = scanner.nextLine().split(", ");
+        int index = 0;
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].length() >= 3 && tokens[i].length() <= 16) {
+                StringBuilder validUsernames = new StringBuilder();
+                for (int j = 0; j < tokens[i].length(); j++) {
+                    if (Character.isAlphabetic(tokens[i].charAt(j))) {
+                        validUsernames.append(tokens[i].charAt(j));
+                    } else if (Character.isDigit(tokens[i].charAt(j))) {
+                        validUsernames.append(tokens[i].charAt(j));
+                    } else if (tokens[i].charAt(j) == '-') {
+                        validUsernames.append(tokens[i].charAt(j));
+                    } else if (tokens[i].charAt(j) == '_') {
+                        validUsernames.append(tokens[i].charAt(j));
+                    }
+                }
+                if (tokens[i].equals(validUsernames.toString())) {
+                    if (index == 0) {
+                        System.out.println("Valid Usernames:");
+                        index++;
+                    }
+                    System.out.println(validUsernames);
+                }
+            }
+        }
     }
 }
