c3aa6f1726ff54fad7a90dc03dea7b41fbc8ef8c
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-22 08:35:27 +0300

    Update Main.java

--- Text Proccessing/p02_Substring/src/Main.java
+++ Text Proccessing/p02_Substring/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+        String string = scanner.nextLine();
+
+        while (string.contains(input)) {
+            string = string.replace(input, "");
+        }
+
+        System.out.println(string);
     }
 }
